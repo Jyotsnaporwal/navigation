a5eb82c8c2ead647b029c9b06e7fbe81192fd8b2
Jyotsnaporwal <45070872+Jyotsnaporwal@users.noreply.github.com>
2021-01-25 11:27:07 +0530

    testing navigation

---
+++ components/HomeScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+          <Button
+          title="Go to About"
+          onPress={() => this.props.navigation.navigate('About'),
+          console.log('pressed')}
+/>
+      </View>
+    )
+  }
+}
